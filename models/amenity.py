c804aa9af63271ee43e4d663f797b6b25d432457
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-10-31 20:12:39 +0100

    Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-
 """Contains the Amenity model"""
-
 from models.base_model import BaseModel
 
 
-
 class Amenity(BaseModel):
-        """Implements the Amenity model"""
-
-            name = ""
+    """Implements the Amenity model"""
+    name = ""
